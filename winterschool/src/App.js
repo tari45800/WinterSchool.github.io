1602791f6f475407b1172ee7236ee098d8b99aa5
DanDanCoding <hazle88105739@gmail.com>
2023-10-06 17:01:38 +0900

    feat: 데이터 베이스 연결

--- winterschool/src/App.js
+++ winterschool/src/App.js
@@ -1,11 +1,29 @@
 import styled from "styled-components";
 import Form from "./components/Form";
+import { useEffect, useState } from "react";
+import { db } from "./Firebase";
+import { collection, getDocs, addDoc } from "firebase/firestore/lite";
 
 const AppContainer = styled.div`
   color: crimson;
 `;
 
 function App() {
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const query = await getDocs(collection(db, "information"));
+        query.forEach((doc) => {
+          console.log(doc.id, doc.data());
+        });
+      } catch (error) {
+        console.error("데이터를 불러오는 중 오류 발생:", error);
+      }
+    };
+
+    getData();
+  }, []);
+
   return (
     <AppContainer>
       <div className="App">2024 김재하 몰입수학 윈터스쿨 신청 폼</div>
--- winterschool/src/components/Form.js
+++ winterschool/src/components/Form.js
@@ -1,22 +1,30 @@
 import { useState } from "react";
 import styled from "styled-components";
+import { db } from "../Firebase";
+import { collection, getDocs, addDoc } from "firebase/firestore/lite";
 
 const FormContainer = styled.div`
   color: Form;
 `;
 
 function Form() {
-  const [formData, setFormData] = useState();
+  const [formData, setFormData] = useState("");
 
   const handleFormData = (e) => {
     setFormData(e.target.value);
   };
 
+  const addData = async () => {
+    addDoc(collection(db, "information"), { 이름: formData });
+    setFormData("");
+  };
+
   return (
     <FormContainer>
       <div>
-        <h1>작성 폼{formData}</h1>
+        <h1>작성 폼</h1>
         <input value={formData} type=" text" onChange={handleFormData} />
+        <button onClick={addData}>등록</button>
       </div>
     </FormContainer>
   );
